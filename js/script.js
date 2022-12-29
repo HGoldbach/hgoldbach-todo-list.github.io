8c4e3d806ceaf50980bbfcb472196c091dff7950
Goldbach <goldbachcs@gmail.com>
2022-12-29 20:24:20 -0300

    Projeto Finalizado

--- js/script.js
+++ js/script.js
@@ -3,37 +3,66 @@ const userInput = document.querySelector("#user-input");
 const fazer = document.querySelector(".fazer");
 const fazendo = document.querySelector(".fazendo");
 const concluida = document.querySelector(".concluido");
+let iconsRemover = document.querySelectorAll('.bi');
+const erroInput = document.querySelector('.user-input-erro');
 
-btn.addEventListener("click", e => {
+
+const criaTarefa = function(){
     if (userInput.value) {
+
+        userInput.style.border = 'none';
+        erroInput.textContent = "";
+        erroInput.style.opacity = '0';
         const li = document.createElement("li");
-        let icon = document.createElement('i');
+        const icon = document.createElement('i');
         icon.classList.add('bi');
         icon.classList.add('bi-trash3-fill');
 
         li.appendChild(document.createTextNode(userInput.value));
         li.append(icon);
 
-        // li.appendChild(document.createTextNode(`${userInput.value} ${<i class="bi bi-trash3-fill"></i>}`));
         fazer.insertAdjacentElement('afterbegin', li);
      
         userInput.value = "";
 
-        li.addEventListener('contextmenu', e => {
-            e.preventDefault();
-            li.remove();
-        })
-        
-        fazer.addEventListener("click", e => {
-            fazendo.appendChild(e.target);
-
-            fazendo.addEventListener("click", e => {
-                concluida.appendChild(e.target);
-
-            })
-        });
+        iconsRemover = document.querySelectorAll('.bi');
+        atualizarIconsRemover();
     } 
+    else {
+        userInput.style.border = '3px solid red';
+        erroInput.textContent = "Você precisa inserir uma tarefa antes de adicionar";
+        erroInput.style.opacity = '1';
+    }
+}
+
+userInput.addEventListener('keypress', e => {
+    if (e.key === "Enter") {
+        criaTarefa();
+    }
 })
+
+btn.addEventListener("click", criaTarefa);
+
+
+fazer.addEventListener("click", e => {
+    fazendo.appendChild(e.target);
+});
+
+
+fazendo.addEventListener("click", e => {
+    concluida.appendChild(e.target);
+})
+
+const atualizarIconsRemover = () => {
+    iconsRemover.forEach(icon => {
+        icon.addEventListener('click', (e) => {
+            const liTarget = e.target.closest('li');
+            liTarget.remove();
+            e.stopPropagation();
+        })
+    })
+};
+
 
 
 
