8876608745690122445ecb58a9502f6ef22e39db
HGoldbach <goldbachcs@gmail.com>
2022-12-29 17:02:07 -0300

    Update layout

--- js/script.js
+++ js/script.js
@@ -1,36 +1,38 @@
-const button = document.querySelector("#btn");
-const input = document.querySelector("#user-input");
+const btn = document.querySelector("#btn");
+const userInput = document.querySelector("#user-input");
 const fazer = document.querySelector(".fazer");
 const fazendo = document.querySelector(".fazendo");
 const concluida = document.querySelector(".concluido");
 
-button.addEventListener("click", e => {
-    e.preventDefault();
-    if (input.value) {
+btn.addEventListener("click", e => {
+    if (userInput.value) {
         const li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        fazer.appendChild(li);
-        input.value = "";
+        let icon = document.createElement('i');
+        icon.classList.add('bi');
+        icon.classList.add('bi-trash3-fill');
 
-        li.addEventListener('contextmenu', (e) => {
+        li.appendChild(document.createTextNode(userInput.value));
+        li.append(icon);
+
+        // li.appendChild(document.createTextNode(`${userInput.value} ${<i class="bi bi-trash3-fill"></i>}`));
+        fazer.insertAdjacentElement('afterbegin', li);
+     
+        userInput.value = "";
+
+        li.addEventListener('contextmenu', e => {
             e.preventDefault();
             li.remove();
         })
         
-        fazer.addEventListener("click", (e) => {
-            const fazerTarget = e.target;
-            if (fazerTarget.tagName.toUpperCase() === "LI") {
-              fazendo.appendChild(fazerTarget);
-            }
-        
-            fazendo.addEventListener("click", (e) => {
-                const fazendoTarget = e.target;
-                if (fazendoTarget.tagName.toUpperCase() === "LI") {
-                    concluida.appendChild(fazendoTarget);
-                }
+        fazer.addEventListener("click", e => {
+            fazendo.appendChild(e.target);
+
+            fazendo.addEventListener("click", e => {
+                concluida.appendChild(e.target);
+
             })
         });
-    }
+    } 
 })
 
 
